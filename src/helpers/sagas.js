8c5eac0c3812b11d4e1eec3cb1b30591502a11a9
Center <>
2022-04-13 12:33:24 +0700

    [SinhVT] remove file

--- src/helpers/sagas.js
+++
@@ -1,5 +0,0 @@
-import { call } from 'redux-saga/effects';
-
-export function callApi(func, ...param) {
-  return call(func, ...param);
-}
--- src/redux/parses/productWithPartner.js
+++
@@ -1,7 +0,0 @@
-export const parseProductWithPartner = (product, partner) => {
-  return {
-    ...product,
-    partnerName: partner?.name,
-    partnerImage: partner?.imageLink
-  };
-};
